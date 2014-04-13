e5d457e02366e5c4a55643482f1735d4dd45b9ae
pauledavey <paul.davey@xtravirt.com>
2014-04-13 09:10:46 +0100

    Updated the program.cs file to run and provide some example output using simple methods in the library. Note that by default a PPT file will be created at the location specified in the presentationFile const variable.

--- Codesharper.PowerPoint.Helper/Codesharper.PowerPoint.Helper.Tester/Program.cs
+++ Codesharper.PowerPoint.Helper/Codesharper.PowerPoint.Helper.Tester/Program.cs
@@ -1,69 +1,145 @@
 ﻿namespace Codesharper.PowerPoint.Helper.Tester
 {
-    using PPT = Microsoft.Office.Interop.PowerPoint;
+    using System.Diagnostics;
+
+    using Codesharper.PowerPoint.Helper.Enumerations;
     using Codesharper.PowerPoint.Helper.Implementations;
-    using PPTHelper = Codesharper.PowerPoint.Helper.Implementations.PresentationManager;
-    using PPTApplication = Codesharper.PowerPoint.Helper.Implementations.PowerPointApplicationManager;
-    using PPTSlideHelper = Codesharper.PowerPoint.Helper.Implementations.SlideManager;
+    using Codesharper.PowerPoint.Helper.Objects;
 
+    using Microsoft.Office.Core;
 
-    class Program
+    using PPT = Microsoft.Office.Interop.PowerPoint;
+
+    internal class Program
     {
-        static void Main(string[] args)
+        private const string presentationFile = @"c:\temp\testPPT.ppt";
+
+        /// <summary>
+        ///     Utility method to determine if a number if odd or even
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static bool IsOdd(int value)
         {
-            // Initial object setups
-            var helper = new PPTHelper();
-            var pptAppHelper = new PPTApplication();
-            var pptApp = pptAppHelper.CreatePowerPointApplication();
-            var pptSlideHelper = new PPTSlideHelper();
-            
-            var shapeHelper = new ShapesManager();
+            return value % 2 != 0;
+        }
 
-            //create a new PPT application instance
-            var presentation = helper.CreatePowerPointPresentation(pptApp, false);
+        private static void Main(string[] args)
+        {
+            // Setup Objects
+            var pptApplicationManager = new PowerPointApplicationManager();
+            var pptSlideManager = new SlideManager();
+            var pptShapeManager = new ShapesManager();
+            var pptPresentationManager = new PresentationManager();
 
-            // add slide to the end of the presentation
-            var slideAtEnd = pptSlideHelper.AddSlideToEnd(presentation);
+            // Step 1. Create a PowerPoint application instance
+            PPT.Application pptApplication = pptApplicationManager.CreatePowerPointApplication();
 
-            // insert slide in to presentation
-            pptSlideHelper.InsertSlide(presentation, (pptSlideHelper.GetSlideCount(presentation) + 1));
-            
-            // grab the first slide in the presentation
-            var mySlide = presentation.Slides[1];
+            // Step 2. Create a new PowerPoint presentation
+            PPT.Presentation pptPresentation = pptPresentationManager.CreatePowerPointPresentation(
+                    pptApplication,
+                    false);
 
-            //// AddTextBox to the the first slide and set some text
-            //var textbox = shapeHelper.AddTextBoxToSlide(mySlide, OFFICE.MsoTextOrientation.msoTextOrientationHorizontal, 100f, 100f, 250f, 250f);
-            //shapeHelper.SetTextBoxText(textbox, "Goat Love Rules Man! YEAH!");
+            // Step 3. Add a slide to the end of the presentation
+            PPT.Slide lastSlide = pptSlideManager.AddSlideToEnd(pptPresentation);
 
-            //var textbox2 = shapeHelper.AddTextBoxToSlide(mySlide, OFFICE.MsoTextOrientation.msoTextOrientationDownward, 400f, 300f, 350f, 350f);
-            //shapeHelper.SetTextBoxText(textbox2, "Going Down!!!");
+            // Step 4. Add a slide to the start of the presentation
+            PPT.Slide firstSlide = pptSlideManager.AddSlideToStart(pptPresentation);
 
-            //// var chart1 = shapeHelper.AddChartToSlide(mySlide, OFFICE.XlChartType.xlLine, 50f, 50f, 50f, 50f);
-            //var objChart = (Graph.Chart)mySlide.Shapes.AddOLEObject(150, 150, 480, 320, "MSGraph.Chart.8", "", OFFICE.MsoTriState.msoFalse, "", 0, "", OFFICE.MsoTriState.msoFalse).OLEFormat.Object;
-            //objChart.ChartType = Graph.XlChartType.xl3DPie;
-            //objChart.Legend.Position = Graph.XlLegendPosition.xlLegendPositionBottom;
-            //objChart.HasTitle = true;
-            //objChart.ChartTitle.Text = "Goats I have loved and known!";
+            // Step 5. Add a slide at position 1 in the presentation
+            PPT.Slide pos1Slide = pptSlideManager.InsertSlide(pptPresentation, 1);
 
-            //try
-            //{
-            //    mySlide.Layout = PPT.PpSlideLayout.ppLayoutChartAndText;
-            //    var objChart = mySlide.Shapes.AddChart(OFFICE.XlChartType.xlLine, 100, 100, 100, 100);
-            //    objChart.Title = "Goats I have loved, lost and known";
-            //}
-            //catch (Exception ex)
-            //{
-            //    throw new Exception("Your Goat has crashed. Stop bleeting and stand it up..");
-            //}
+            // Step 6a. Add a text box to each of the slides showing the slide number
+            foreach (PPT.Slide slide in pptPresentation.Slides)
+            {
+                PPT.Shape textboxShape = pptShapeManager.AddTextBoxToSlide(
+                        slide,
+                        MsoTextOrientation.msoTextOrientationHorizontal,
+                        200f,
+                        200f,
+                        200f,
+                        200f);
 
+                pptShapeManager.SetTextBoxText(textboxShape, "This is slide " + slide.SlideIndex);
 
-            var objTable = mySlide.Shapes.AddTable(10, 3, 100f, 100f, 300f, 300f);
+                // Step 6b. Add a shape to the slide; star if its an odd numbered slide, rectanle if its an even numbered slide
+                if (IsOdd(slide.SlideIndex))
+                {
+                    PPT.Shape starShape = pptShapeManager.DrawShape(
+                            slide,
+                            MsoAutoShapeType.msoShape8pointStar,
+                            400f,
+                            150f,
+                            250f,
+                            250f);
+                }
+                else
+                {
+                    PPT.Shape rectangleSHape = pptShapeManager.DrawShape(
+                            slide,
+                            MsoAutoShapeType.msoShapeRectangle,
+                            400f,
+                            150f,
+                            250f,
+                            250f);
+                }
 
+                // Step 6c. Add a comment to the slide telling you if the index of the slide is odd or even
+                string slideCommentText = string.Empty;
 
+                if (IsOdd(slide.SlideIndex))
+                {
+                    slideCommentText = @"This is an odd numbered slide";
+                }
+                else
+                {
+                    slideCommentText = @"This is an even numbered slide";
+                }
 
-            // save and close powerpoint
-            helper.SavePresentationAs(presentation, @"c:\example.ppt", PPT.PpSaveAsFileType.ppSaveAsPresentation, true);
-            helper.ClosePresentation(presentation);
+                PPT.Slide slideComment = pptSlideManager.AddComment(
+                        slide,
+                        new SlideComment
+                            {
+                                    Author = "Test Author",
+                                    AuthorInitials = "TA",
+                                    Comment = slideCommentText,
+                                    LeftPosition = 200f,
+                                    TopPosition = 50f
+                            });
+            }
+
+            // Step 7. Clone the first slide and make it the last slide
+            PPT.SlideRange cloneSlide = pptSlideManager.CloneSlide(pptPresentation, firstSlide, Locations.Location.Last);
+
+            // Step 8a. Add a new 'summary' slide to the end of the presentation
+            PPT.Slide summarySlide = pptSlideManager.AddSlideToEnd(pptPresentation);
+
+            // Step 8b. Add a count to the slide showing the total number of slides in the presentation
+            PPT.Shape summarySlideCountText = pptShapeManager.AddTextBoxToSlide(
+                    summarySlide,
+                    MsoTextOrientation.msoTextOrientationHorizontal,
+                    300f,
+                    300f,
+                    300f,
+                    300f);
+
+            // Step 8c. Set the text to the number of slides in the presentation
+            pptShapeManager.SetTextBoxText(
+                    summarySlideCountText,
+                    "There are " + pptSlideManager.GetSlideCount(pptPresentation).ToString() + " slides in this presentation!");
+
+            // Move the summary slide to be the first slide in the presentation
+            pptSlideManager.MoveSlide(pptPresentation, summarySlide, Locations.Location.First);
+
+            // Step 9. Save the presentation to c:\temp\testPPT.pptx and open it
+            pptPresentationManager.SavePresentationAs(
+                    pptPresentation,
+                    presentationFile,
+                    PPT.PpSaveAsFileType.ppSaveAsPresentation,
+                    true);
+            pptPresentationManager.ClosePresentation(pptPresentation);
+
+            Process.Start(presentationFile);
         }
     }
 }
